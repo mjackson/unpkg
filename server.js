eeb7b62dcef131bfbeda8b75026c9220fe5b3812
Michael Jackson <mjijackson@gmail.com>
2016-02-21 18:52:19 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var port = process.env.PORT || 5000
+
+var express = require('express')
+var createRequestHandler = require('npm-http-server').createRequestHandler
+var cors = require('cors')
+
+var app = express()
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(express.static('public', { maxAge: 60000 }))
+app.use(
+  createRequestHandler()
+)
+
+app.listen(port, function () {
+  console.log('Server started on port ' + port + '. Ctrl+C to quit')
+})
