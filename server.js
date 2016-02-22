dc1a5d1f7d3e6a1ed506470f235546fe23af596b
Michael Jackson <mjijackson@gmail.com>
2016-02-21 19:00:11 -0800

    Add port npm config var

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-var port = process.env.PORT || 5000
-
 var express = require('express')
 var createRequestHandler = require('npm-http-server').createRequestHandler
 var cors = require('cors')
 
+var port = process.env.PORT || process.env.npm_package_config_port
 var app = express()
 
 app.disable('x-powered-by')
 app.use(cors())
 app.use(express.static('public', { maxAge: 60000 }))
-app.use(
-  createRequestHandler()
-)
+app.use(createRequestHandler())
 
 app.listen(port, function () {
   console.log('Server started on port ' + port + '. Ctrl+C to quit')
