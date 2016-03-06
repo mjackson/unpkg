187ee39b1cc28c04d702a8e70f6efcf4188e8ea9
Michael Jackson <mjijackson@gmail.com>
2016-03-05 21:59:53 -0800

    Add more npm config vars

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const createRequestHandler = require('npm-http-server').createRequestHandler
 const express = require('express')
 const cors = require('cors')
 
+const registryURL = process.env.npm_package_config_registryURL
+const bowerBundle = process.env.npm_package_config_bowerBundle
 const port = process.env.PORT || process.env.npm_package_config_port
 const app = express()
 
 app.disable('x-powered-by')
 app.use(cors())
 app.use(express.static('public', { maxAge: 60000 }))
-app.use(createRequestHandler())
+app.use(createRequestHandler(
+  registryURL,
+  bowerBundle
+))
 
 app.listen(port, function () {
-  console.log('Server started on port ' + port + '. Ctrl+C to quit')
+  console.log('Server started on port %s. Ctrl+C to quit', port)
 })
