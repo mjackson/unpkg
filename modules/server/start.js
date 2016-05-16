5f2615f2eda4138a568e4a3afad36628cc74adaf
Michael Jackson <mjijackson@gmail.com>
2016-05-16 15:12:49 -0700

    Do not use Babel's register hook in production

--- modules/server/start.js
+++ modules/server/start.js
@@ -1,8 +1,4 @@
-require('babel-register')({
-  only: require('path').resolve(__dirname, './modules')
-})
-
-const createServer = require('./modules/ServerUtils').createServer
+import { createServer } from './index'
 
 const port = process.env.PORT || 5000
 const registryURL = process.env.REGISTRY_URL || 'https://registry.npmjs.org'
@@ -12,11 +8,11 @@ const autoIndex = !process.env.DISABLE_INDEX
 const redisURL = process.env.REDIS_URL
 
 const server = createServer({
-  registryURL: registryURL,
-  bowerBundle: bowerBundle,
-  redirectTTL: redirectTTL,
-  autoIndex: autoIndex,
-  redisURL: redisURL
+  registryURL,
+  bowerBundle,
+  redirectTTL,
+  autoIndex,
+  redisURL
 })
 
 server.listen(port, () => {
