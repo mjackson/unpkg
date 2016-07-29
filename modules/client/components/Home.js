dde88c539ba2ed431607df7d137f05d03c500ccd
Michael Jackson <mjijackson@gmail.com>
2016-07-29 15:15:41 -0700

    Syntax tweak

--- modules/client/components/Home.js
+++ modules/client/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 import contentHTML from './Home.md'
 
 class Home extends React.Component {
-  render = () =>
-    <div className="wrapper" dangerouslySetInnerHTML={{ __html: contentHTML }}/>
+  render() {
+    return (
+      <div className="wrapper" dangerouslySetInnerHTML={{ __html: contentHTML }}/>
+    )
+  }
 }
 
 export default Home
