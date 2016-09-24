54011c9a5e9db4770dd13e43ac4059538db86bd8
Michael Jackson <mjijackson@gmail.com>
2016-09-24 10:21:11 -0700

    Remove unused file

--- modules/server/StyleUtils.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-import csso from 'csso'
-
-export const readCSS = (...args) =>
-  minifyCSS(fs.readFileSync(path.resolve.apply(path, args), 'utf8'))
-
-export const minifyCSS = (css) =>
-  csso.minify(css).css
