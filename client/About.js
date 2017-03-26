25e4a77fb2f7088081a25defeff74f4b5dadbb29
MICHAEL JACKSON <mjijackson@gmail.com>
2017-03-26 00:17:43 -0700

    Update method definition

--- client/About.js
+++ client/About.js
@@ -2,8 +2,11 @@ import React from 'react'
 import contentHTML from './About.md'
 
 class About extends React.Component {
-  render = () =>
-    <div className="wrapper" dangerouslySetInnerHTML={{ __html: contentHTML }}/>
+  render() {
+    return (
+      <div className="wrapper" dangerouslySetInnerHTML={{ __html: contentHTML }}/>
+    )
+  }
 }
 
 export default About
