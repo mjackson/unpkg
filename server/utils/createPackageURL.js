a8ab0b7dabfe1e4f0d8625dcb3d9ba18a0ca8220
MICHAEL JACKSON <mjijackson@gmail.com>
2017-11-08 09:13:28 -0800

    Fix failing tests

--- server/utils/createPackageURL.js
+++ server/utils/createPackageURL.js
@@ -1,12 +1,8 @@
 function createPackageURL(packageName, version, filename, search) {
   let pathname = `/${packageName}`
-
   if (version != null) pathname += `@${version}`
-
   if (filename) pathname += filename
-
   if (search) pathname += search
-
   return pathname
 }
 
