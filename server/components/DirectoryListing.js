1c16b4fad71d722a97ddb175f069e97fa4751673
Felix Wu <mail@flxwu.com>
2018-05-22 22:26:30 +0200

    fix double declaration

--- server/components/DirectoryListing.js
+++ server/components/DirectoryListing.js
@@ -12,7 +12,7 @@ function DirectoryListing({ dir, entries }) {
   const rows = entries.map(({ file, stats }, index) => {
     const isDir = stats.isDirectory();
     const href = file + (isDir ? "/" : "");
-    const file = file + (isDir ? "/" : "");
+    file = file + (isDir ? "/" : "");
 
     return e(
       "tr",
