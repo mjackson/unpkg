0f895bf04e5187f121a0d3aa06ee2e4d0a54204b
Michael Jackson <mj@mjackson.me>
2018-05-22 22:06:55 -0400

    Use lockfile to prevent inter-process races
    See #86

--- server/utils/fetchPackage.js
+++ server/utils/fetchPackage.js
@@ -1,10 +1,6 @@
 require("isomorphic-fetch");
-const fs = require("fs");
-const mkdirp = require("mkdirp");
 const gunzip = require("gunzip-maybe");
 const tar = require("tar-fs");
-
-const createTempPath = require("./createTempPath");
 
 function stripNamePrefix(headers) {
   // Most packages have header names that look like "package/index.js"
@@ -35,38 +31,10 @@ function extractResponse(response, outputDir) {
   });
 }
 
-function fetchPackage(packageConfig) {
-  return new Promise((resolve, reject) => {
-    const tarballURL = packageConfig.dist.tarball;
-    const outputDir = createTempPath(packageConfig.name, packageConfig.version);
+function fetchPackage(tarballURL, outputDir) {
+  console.log(`info: Fetching ${tarballURL} and extracting to ${outputDir}`);
 
-    console.log(`info: Fetching ${tarballURL} and extracting to ${outputDir}`);
-
-    fs.access(outputDir, error => {
-      if (error) {
-        if (error.code === "ENOENT" || error.code === "ENOTDIR") {
-          // ENOENT or ENOTDIR are to be expected when we haven't yet
-          // fetched a package for the first time. Carry on!
-          mkdirp(outputDir, error => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(
-                fetch(tarballURL)
-                  .then(res => extractResponse(res, outputDir))
-                  .then(() => outputDir)
-              );
-            }
-          });
-        } else {
-          reject(error);
-        }
-      } else {
-        // Best case: we already have this package cached on disk!
-        resolve(outputDir);
-      }
-    });
-  });
+  return fetch(tarballURL).then(res => extractResponse(res, outputDir));
 }
 
 module.exports = fetchPackage;
