cde97034bf68df05874b6abf263467f01e7e618a
Felix Wu <mail@flxwu.com>
2018-05-28 15:11:21 +0200

    return to spaces instead of tabs

--- server/actions/serveFile.js
+++ server/actions/serveFile.js
@@ -21,153 +21,153 @@ const AutoIndex = !process.env.DISABLE_INDEX;
 const MaximumDepth = 128;
 
 function serveMetadata(req, res) {
-	getMetadata(
-		req.packageDir,
-		req.filename,
-		req.stats,
-		MaximumDepth,
-		(error, metadata) => {
-			if (error) {
-				console.error(error);
+  getMetadata(
+    req.packageDir,
+    req.filename,
+    req.stats,
+    MaximumDepth,
+    (error, metadata) => {
+      if (error) {
+        console.error(error);
 
-				res
-					.status(500)
-					.type("text")
-					.send(
-						`Cannot generate metadata for ${req.packageSpec}${req.filename}`
-					);
-			} else {
-				// Cache metadata for 1 year.
-				res
-					.set({
-						"Cache-Control": "public, max-age=31536000",
-						"Cache-Tag": "meta"
-					})
-					.send(metadata);
-			}
-		}
-	);
+        res
+          .status(500)
+          .type("text")
+          .send(
+            `Cannot generate metadata for ${req.packageSpec}${req.filename}`
+          );
+      } else {
+        // Cache metadata for 1 year.
+        res
+          .set({
+            "Cache-Control": "public, max-age=31536000",
+            "Cache-Tag": "meta"
+          })
+          .send(metadata);
+      }
+    }
+  );
 }
 
 function rewriteBareModuleIdentifiers(file, packageConfig, callback) {
-	const dependencies = Object.assign(
-		{},
-		packageConfig.peerDependencies,
-		packageConfig.dependencies
-	);
+  const dependencies = Object.assign(
+    {},
+    packageConfig.peerDependencies,
+    packageConfig.dependencies
+  );
 
-	const options = {
-		// Ignore .babelrc and package.json babel config
-		// because we haven't installed dependencies so
-		// we can't load plugins; see #84
-		babelrc: false,
-		plugins: [unpkgRewrite(dependencies)]
-	};
+  const options = {
+    // Ignore .babelrc and package.json babel config
+    // because we haven't installed dependencies so
+    // we can't load plugins; see #84
+    babelrc: false,
+    plugins: [unpkgRewrite(dependencies)]
+  };
 
-	babel.transformFile(file, options, (error, result) => {
-		callback(error, result && result.code);
-	});
+  babel.transformFile(file, options, (error, result) => {
+    callback(error, result && result.code);
+  });
 }
 
 function serveJavaScriptModule(req, res) {
-	if (getFileContentType(req.filename) !== "application/javascript") {
-		return res
-			.status(403)
-			.type("text")
-			.send("?module mode is available only for JavaScript files");
-	}
+  if (getFileContentType(req.filename) !== "application/javascript") {
+    return res
+      .status(403)
+      .type("text")
+      .send("?module mode is available only for JavaScript files");
+  }
 
-	const file = path.join(req.packageDir, req.filename);
+  const file = path.join(req.packageDir, req.filename);
 
-	rewriteBareModuleIdentifiers(file, req.packageConfig, (error, code) => {
-		if (error) {
-			console.error(error);
+  rewriteBareModuleIdentifiers(file, req.packageConfig, (error, code) => {
+    if (error) {
+      console.error(error);
 
-			const errorName = error.constructor.name;
-			const errorMessage = error.message.replace(
-				/^.*?\/unpkg-.+?\//,
-				`/${req.packageSpec}/`
-			);
-			const codeFrame = error.codeFrame;
-			const debugInfo = `${errorName}: ${errorMessage}\n\n${codeFrame}`;
+      const errorName = error.constructor.name;
+      const errorMessage = error.message.replace(
+        /^.*?\/unpkg-.+?\//,
+        `/${req.packageSpec}/`
+      );
+      const codeFrame = error.codeFrame;
+      const debugInfo = `${errorName}: ${errorMessage}\n\n${codeFrame}`;
 
-			res
-				.status(500)
-				.type("text")
-				.send(
-					`Cannot generate module for ${req.packageSpec}${
-						req.filename
-					}\n\n${debugInfo}`
-				);
-		} else {
-			// Cache modules for 1 year.
-			res
-				.set({
-					"Content-Type": "application/javascript; charset=utf-8",
-					"Content-Length": Buffer.byteLength(code),
-					"Cache-Control": "public, max-age=31536000",
-					"Cache-Tag": "file,js-file,js-module"
-				})
-				.send(code);
-		}
-	});
+      res
+        .status(500)
+        .type("text")
+        .send(
+          `Cannot generate module for ${req.packageSpec}${
+            req.filename
+          }\n\n${debugInfo}`
+        );
+    } else {
+      // Cache modules for 1 year.
+      res
+        .set({
+          "Content-Type": "application/javascript; charset=utf-8",
+          "Content-Length": Buffer.byteLength(code),
+          "Cache-Control": "public, max-age=31536000",
+          "Cache-Tag": "file,js-file,js-module"
+        })
+        .send(code);
+    }
+  });
 }
 
 function serveStaticFile(req, res) {
-	// look whether html param exists
-	const html = req.query.html != undefined ? true : false;
-	console.log(
-		`Trying to send ${req.packageSpec}${req.filename} ${
-			html ? "as html page" : "as file"
-		}`
-	);
-	const tags = ["file"];
+  // look whether html param exists
+  const html = req.query.html != undefined ? true : false;
+  console.log(
+    `Trying to send ${req.packageSpec}${req.filename} ${
+      html ? "as html page" : "as file"
+    }`
+  );
+  const tags = ["file"];
 
-	const ext = path.extname(req.filename).substr(1);
-	if (ext) {
-		tags.push(`${ext}-file`);
-	}
+  const ext = path.extname(req.filename).substr(1);
+  if (ext) {
+    tags.push(`${ext}-file`);
+  }
 
-	let contentType = getFileContentType(req.filename);
-	if (contentType === "application/javascript") {
-		contentType += "; charset=utf-8";
-	}
+  let contentType = getFileContentType(req.filename);
+  if (contentType === "application/javascript") {
+    contentType += "; charset=utf-8";
+  }
 
-	// Cache files for 1 year.
-	res.set({
-		"Content-Length": req.stats.size,
-		"Cache-Control": "public, max-age=31536000",
-		"Last-Modified": req.stats.mtime.toUTCString(),
-		ETag: etag(req.stats),
-		"Cache-Tag": tags.join(",")
-	});
+  // Cache files for 1 year.
+  res.set({
+    "Content-Length": req.stats.size,
+    "Cache-Control": "public, max-age=31536000",
+    "Last-Modified": req.stats.mtime.toUTCString(),
+    ETag: etag(req.stats),
+    "Cache-Tag": tags.join(",")
+  });
 
-	const file = path.join(req.packageDir, req.filename);
-	const stream = fs.createReadStream(file);
+  const file = path.join(req.packageDir, req.filename);
+  const stream = fs.createReadStream(file);
 
-	stream.on("error", error => {
-		console.error(`Cannot send file ${req.packageSpec}${req.filename}`);
-		console.error(error);
-		res.sendStatus(500);
-	});
+  stream.on("error", error => {
+    console.error(`Cannot send file ${req.packageSpec}${req.filename}`);
+    console.error(error);
+    res.sendStatus(500);
+  });
 
-	let chunks = [];
-	let codeString = "";
-	// IF HTML: push file content chunk-wise from stream to array
-	stream.on("data", chunk => {
-		if (html) {
-			chunks.push(chunk);
-		}
-	});
+  let chunks = [];
+  let codeString = "";
+  // IF HTML: push file content chunk-wise from stream to array
+  stream.on("data", chunk => {
+    if (html) {
+      chunks.push(chunk);
+    }
+  });
 
-	// IF HTML: return syntax highlighted code as html
-	stream.on("close", () => {
-		if (html) {
-			// turn chunk-array to string
-			codeString = Buffer.concat(chunks).toString();
-			// html template using highlight.js
-			let highlightStyle = "default";
-			let html = ` 
+  // IF HTML: return syntax highlighted code as html
+  stream.on("close", () => {
+    if (html) {
+      // turn chunk-array to string
+      codeString = Buffer.concat(chunks).toString();
+      // html template using highlight.js
+      let highlightStyle = "default";
+      let html = ` 
 			<html>
 			<head>
 			<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/${highlightStyle}.min.css">
@@ -182,68 +182,68 @@ function serveStaticFile(req, res) {
 			</html>
 			`;
 
-			// send back html
-			res.send(html);
-		}
-	});
+      // send back html
+      res.send(html);
+    }
+  });
 
-	// send back the file if there is no html parameter given
-	if (!html) {
-		stream.pipe(res);
-	}
+  // send back the file if there is no html parameter given
+  if (!html) {
+    stream.pipe(res);
+  }
 }
 
 function serveIndex(req, res) {
-	const dir = path.join(req.packageDir, req.filename);
+  const dir = path.join(req.packageDir, req.filename);
 
-	getEntries(dir).then(
-		entries => {
-			const html = renderPage(IndexPage, {
-				packageInfo: req.packageInfo,
-				version: req.packageVersion,
-				dir: req.filename,
-				entries
-			});
+  getEntries(dir).then(
+    entries => {
+      const html = renderPage(IndexPage, {
+        packageInfo: req.packageInfo,
+        version: req.packageVersion,
+        dir: req.filename,
+        entries
+      });
 
-			// Cache HTML directory listings for 1 minute.
-			res
-				.set({
-					"Cache-Control": "public, max-age=60",
-					"Cache-Tag": "index"
-				})
-				.send(html);
-		},
-		error => {
-			console.error(error);
+      // Cache HTML directory listings for 1 minute.
+      res
+        .set({
+          "Cache-Control": "public, max-age=60",
+          "Cache-Tag": "index"
+        })
+        .send(html);
+    },
+    error => {
+      console.error(error);
 
-			res
-				.status(500)
-				.type("text")
-				.send(`Cannot read entries for ${req.packageSpec}${req.filename}`);
-		}
-	);
+      res
+        .status(500)
+        .type("text")
+        .send(`Cannot read entries for ${req.packageSpec}${req.filename}`);
+    }
+  );
 }
 
 /**
  * Send the file, JSON metadata, or HTML directory listing.
  */
 function serveFile(req, res) {
-	if (req.query.meta != null) {
-		serveMetadata(req, res);
-	} else if (req.stats.isFile()) {
-		if (req.query.module != null) {
-			serveJavaScriptModule(req, res);
-		} else {
-			serveStaticFile(req, res);
-		}
-	} else if (req.stats.isDirectory() && AutoIndex) {
-		serveIndex(req, res);
-	} else {
-		res
-			.status(403)
-			.type("text")
-			.send(`Cannot serve ${req.packageSpec}${req.filename}; it's not a file`);
-	}
+  if (req.query.meta != null) {
+    serveMetadata(req, res);
+  } else if (req.stats.isFile()) {
+    if (req.query.module != null) {
+      serveJavaScriptModule(req, res);
+    } else {
+      serveStaticFile(req, res);
+    }
+  } else if (req.stats.isDirectory() && AutoIndex) {
+    serveIndex(req, res);
+  } else {
+    res
+      .status(403)
+      .type("text")
+      .send(`Cannot serve ${req.packageSpec}${req.filename}; it's not a file`);
+  }
 }
 
 module.exports = serveFile;
