cb47fbf775dd21622864c0e7dcd79f8482740894
Michael Jackson <mj@mjackson.me>
2018-06-08 23:01:13 -0700

    Add secretKey module

--- server/AuthAPI.js
+++ server/AuthAPI.js
@@ -1,29 +1,8 @@
-const fs = require("fs");
-const path = require("path");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const invariant = require("invariant");
-const forge = require("node-forge");
 
 const db = require("./utils/redis");
-
-let keys;
-if (process.env.NODE_ENV === "production") {
-  keys = {
-    public: fs.readFileSync(path.resolve(__dirname, "../public.key"), "utf8"),
-    private: process.env.PRIVATE_KEY
-  };
-
-  invariant(keys.private, "Missing $PRIVATE_KEY environment variable");
-} else {
-  // Generate a random keypair for dev/testing.
-  // See https://gist.github.com/sebadoom/2b70969e70db5da9a203bebd9cff099f
-  const keypair = forge.rsa.generateKeyPair({ bits: 2048 });
-  keys = {
-    public: forge.pki.publicKeyToPem(keypair.publicKey, 72),
-    private: forge.pki.privateKeyToPem(keypair.privateKey, 72)
-  };
-}
+const secretKey = require("./secretKey");
 
 function getCurrentSeconds() {
   return Math.floor(Date.now() / 1000);
@@ -42,13 +21,18 @@ function createToken(scopes = {}) {
       scopes
     };
 
-    jwt.sign(payload, keys.private, { algorithm: "RS256" }, (error, token) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(token);
+    jwt.sign(
+      payload,
+      secretKey.private,
+      { algorithm: "RS256" },
+      (error, token) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(token);
+        }
       }
-    });
+    );
   });
 }
 
@@ -58,7 +42,7 @@ function verifyToken(token) {
   return new Promise((resolve, reject) => {
     const options = { algorithms: ["RS256"] };
 
-    jwt.verify(token, keys.public, options, (error, payload) => {
+    jwt.verify(token, secretKey.public, options, (error, payload) => {
       if (error) {
         reject(error);
       } else {
@@ -107,7 +91,7 @@ function removeAllRevokedTokens() {
 }
 
 function getPublicKey() {
-  return keys.public;
+  return secretKey.public;
 }
 
 module.exports = {
---
+++ server/secretKey.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const forge = require("node-forge");
+const invariant = require("invariant");
+
+let secretKey;
+if (process.env.NODE_ENV === "production") {
+  invariant(
+    process.env.PRIVATE_KEY,
+    "Missing $PRIVATE_KEY environment variable"
+  );
+
+  secretKey = {
+    public: fs.readFileSync(
+      path.resolve(__dirname, "../secret_key.pub"),
+      "utf8"
+    ),
+    private: process.env.PRIVATE_KEY
+  };
+} else {
+  // Generate a random keypair for dev/testing.
+  // See https://gist.github.com/sebadoom/2b70969e70db5da9a203bebd9cff099f
+  const keypair = forge.rsa.generateKeyPair({ bits: 2048 });
+
+  secretKey = {
+    public: forge.pki.publicKeyToPem(keypair.publicKey, 72),
+    private: forge.pki.privateKeyToPem(keypair.privateKey, 72)
+  };
+}
+
+module.exports = secretKey;
