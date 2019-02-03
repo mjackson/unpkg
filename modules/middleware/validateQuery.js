bbbbbcbeff10b729cfb4875c241a4f323cb4129f
Michael Jackson <michael@jackson.us>
2019-02-02 17:30:49 -0800

    Small refactor

--- modules/middleware/validateQuery.js
+++ modules/middleware/validateQuery.js
@@ -10,23 +10,23 @@ function isKnownQueryParam(param) {
   return !!knownQueryParams[param];
 }
 
-function sanitizeQuery(originalQuery) {
-  const query = {};
-
-  Object.keys(originalQuery).forEach(param => {
-    if (isKnownQueryParam(param)) query[param] = originalQuery[param];
-  });
-
-  return query;
-}
-
 /**
  * Reject URLs with invalid query parameters to increase cache hit rates.
  */
 export default function validateQuery(req, res, next) {
-  if (!Object.keys(req.query).every(isKnownQueryParam)) {
-    return res.redirect(302, req.path + createSearch(sanitizeQuery(req.query)));
+  const keys = Object.keys(req.query);
+
+  if (keys.every(isKnownQueryParam)) {
+    return next();
   }
 
-  next();
+  const newQuery = {};
+
+  keys.forEach(param => {
+    if (isKnownQueryParam(param)) {
+      newQuery[param] = req.query[param];
+    }
+  });
+
+  return res.redirect(302, req.path + createSearch(newQuery));
 }
