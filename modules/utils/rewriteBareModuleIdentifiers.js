83bc3470eb8ff2d4e09ccbf565ae783e72788c87
Jason Miller <developit@google.com>
2019-03-06 11:09:29 -0500

    Preserve minification semantics when transforming bare import specifiers (fixes #149)

--- modules/utils/rewriteBareModuleIdentifiers.js
+++ modules/utils/rewriteBareModuleIdentifiers.js
@@ -16,6 +16,10 @@ export default function rewriteBareModuleIdentifiers(code, packageConfig) {
     // because we haven't installed dependencies so
     // we can't load plugins; see #84
     babelrc: false,
+    // Make a reasonable attempt to preserve whitespace
+    // from the original file. This ensures minified
+    // .mjs stays minified; see #149
+    retainLines: true,
     plugins: [unpkgRewrite(origin, dependencies)]
   };
 
