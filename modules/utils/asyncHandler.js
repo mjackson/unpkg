370a7f7d6c0e58afff217c5906e3ab0c657e5d93
Michael Jackson <michael@jackson.us>
2019-08-04 23:49:34 -0700

    Add request logging to asyncHandler

--- modules/utils/asyncHandler.js
+++ modules/utils/asyncHandler.js
@@ -1,12 +1,12 @@
 /**
- * Useful for wrapping `async` request handlers so they
- * automatically propagate errors.
+ * Useful for wrapping `async` request handlers in Express
+ * so they automatically propagate errors.
  */
 export default function asyncHandler(handler) {
   return (req, res, next) => {
     Promise.resolve(handler(req, res, next)).catch(error => {
-      console.error(`Unexpected error in ${handler.name}!`);
-      console.error(error);
+      req.log.error(`Unexpected error in ${handler.name}!`);
+      req.log.error(error.stack);
 
       next(error);
     });
