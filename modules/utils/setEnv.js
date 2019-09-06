b1211b58411608838a73b9103192a73537484a9a
KevinSheedy <kevinsheedy@gmail.com>
2019-09-06 11:01:16 +0100

    indentation fix

--- modules/utils/setEnv.js
+++ modules/utils/setEnv.js
@@ -3,13 +3,13 @@ const argv = require('minimist')(process.argv.slice(2));
 const { _, ...flags } = argv;
 
 Object.keys(flags || {}).map(key => {
-	const value = flags[key];
-	process.env[key] = value;
+  const value = flags[key];
+  process.env[key] = value;
 });
 
 process.env.ENABLE_CLOUDFLARE =
-	flags.ENABLE_CLOUDFLARE === 'true' ? true : false;
+  flags.ENABLE_CLOUDFLARE === 'true' ? true : false;
 
 export default function() {
-	return _;
+  return _;
 }
